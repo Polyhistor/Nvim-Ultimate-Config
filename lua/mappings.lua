7546690dde426dac3d392cae6631fa41a1697196
Pouya <pouya.ataei.7@gmail.com>
2024-09-04 19:54:59 +1200

    uddated the mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,3 +7,4 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("t",'<Esc>', '<C-\\><C-n>')
+map("n", "<leader>d","_d", {desc = "delete without yanking"})
